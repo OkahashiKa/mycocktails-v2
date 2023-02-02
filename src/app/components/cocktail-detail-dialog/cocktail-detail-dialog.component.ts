695a3eeec8f80f629bf7d7aa20fab7eb5a5cdc88
kazuki.okahashi <okarians.302.dev@gmail.com>
2023-02-03 08:45:09 +0900

    feat: 画像をSupabaseから取得

--- src/app/components/cocktail-detail-dialog/cocktail-detail-dialog.component.ts
+++ src/app/components/cocktail-detail-dialog/cocktail-detail-dialog.component.ts
@@ -8,6 +8,9 @@ import { CocktailDetailDialogData } from '@/components/search-user-cocktails/sea
   styleUrls: ['./cocktail-detail-dialog.component.css'],
 })
 export class CocktailDetailDialogComponent implements OnInit {
+  public readonly nonImageURL: string =
+    'https://erpbbaxjnffnjoheuupq.supabase.co/storage/v1/object/public/images/noimage-760x460.png?t=2023-02-02T23%3A16%3A19.502Z';
+
   constructor(
     public dialogRef: MatDialogRef<CocktailDetailDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: CocktailDetailDialogData
