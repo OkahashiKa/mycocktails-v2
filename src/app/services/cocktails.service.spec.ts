6a318f1b8997dee1c7607799451064bbaeee815f
kazuki.okahashi <okarians.302.dev@gmail.com>
2023-02-09 09:09:40 +0900

    feat: cocktail.serviseのUT実装

--- src/app/services/cocktails.service.spec.ts
+++ src/app/services/cocktails.service.spec.ts
@@ -1,15 +1,84 @@
 import { TestBed } from '@angular/core/testing';
 import { CocktailsService } from '@/services/cocktails.service';
+import { CocktailModel } from '@/models/cocktail/cocktailModel';
+import { SupabaseCocktailService } from '@/services/supabase/supabase-cocktail.service';
+import { CocktailResipeModel } from '@/models/cocktail/cocktailRecipeModel';
 
-describe('CocktailsService', () => {
+fdescribe('CocktailsService', () => {
   let service: CocktailsService;
 
+  // @note テストデータ
+  let cocktailIdWithUserMaterialInRecipe: number[] = [];
+  let cocktailRecipeList: CocktailResipeModel[] = [];
+  let userCocktailList: CocktailModel[] = [];
+
+  // @note: サービスのモック
+  const supabaseCocktailServiceMock = {
+    getCocktailIdWithUserMaterialInRecipe: async () =>
+      await cocktailIdWithUserMaterialInRecipe,
+    getCocktailRecipe: async () => await cocktailRecipeList,
+    getCocktailByIdList: async () => await userCocktailList,
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: SupabaseCocktailService,
+          useValue: supabaseCocktailServiceMock,
+        },
+      ],
+    });
     service = TestBed.inject(CocktailsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getUserCocktailsList', () => {
+    describe('ユーザー材料を含むレシピが存在しない', () => {
+      it('空配列が取得されること', () => {
+        service.getUserCocktailsList([]).subscribe((x) => {
+          expect(x).toEqual([]);
+        });
+      });
+    });
+    describe('ユーザー材料を含むレシピが存在する', () => {
+      beforeEach(() => {
+        cocktailIdWithUserMaterialInRecipe = [1, 2];
+      });
+      describe('レシピ情報が存在しない', () => {
+        it('空配列が取得されること', () => {
+          service.getUserCocktailsList([]).subscribe((x) => {
+            expect(x).toEqual([]);
+          });
+        });
+      });
+      describe('レシピ情報が存在する', () => {
+        beforeEach(() => {
+          cocktailRecipeList = [{ cocktail_id: 1, material_id: 1 }];
+        });
+        describe('ユーザーが作成可能なカクテルが存在しない', () => {
+          it('空配列が取得されること', () => {
+            service.getUserCocktailsList([]).subscribe((x) => {
+              expect(x).toEqual([]);
+            });
+          });
+        });
+        describe('ユーザーが作成可能なカクテルが存在する', () => {
+          beforeEach(() => {
+            userCocktailList = [
+              { id: 1, name: 'test', remarks: '', image: '' },
+            ];
+          });
+          it('カクテル情報が取得されること', () => {
+            service.getUserCocktailsList([]).subscribe((x) => {
+              expect(x).toEqual(userCocktailList);
+            });
+          });
+        });
+      });
+    });
   });
 });
